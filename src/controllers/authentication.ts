62becbd730dd50941e9a66905877c9bbf7a59c05
agrimes23 <107214142+agrimes23@users.noreply.github.com>
2024-05-01 19:49:38 -0500

    setting up authentication controllers

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -6,6 +6,32 @@ import { authentication, random } from '../helpers'
 export const login = async (req: express.Request, res: express.Response) => {
     try {
         const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.sendStatus(400)
+        }
+
+        const user = await getUserByEmail(email).select('+authentication.salt +authentication.password')
+
+        if (!user) {
+            return res.sendStatus(400)
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password)
+
+        if (user.authentication.password !== expectedHash){
+            return res.sendStatus(403)
+        }
+
+        const salt = random();
+        user.authentication.sessionToken = authentication(salt, user._id.toString())
+
+        await user.save();
+
+        res.cookie('JLPT-NEWS-AUTH', user.authentication.sessionToken, { domain: 'localhost', path: '/'})
+
+        return res.status(200).json(user).end();
+        
     } catch (error) {
         console.log(error)
         return res.sendStatus(400)
@@ -16,7 +42,7 @@ export const register = async (req: express.Request, res: express.Response) => {
     try{
         const { firstName, lastName, email, password } = req.body
 
-        if (!email || !password || ! firstName) {
+        if (!email || !password || !firstName) {
             return res.sendStatus(400)
         }
 
